b6f5236f679ed3fc769d6858f832f454772255b9
Vercel <vercel[bot]@users.noreply.github.com>
2026-08-19 07:34:52 +0000

    Install Vercel Speed Insights package
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'CS App',
+  description: 'Created with Next.js',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  )
+}
